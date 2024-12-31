4b2f09b1b7fa11819acde400acc17ead59c8b105
Romain Roffé <rofferom@gmail.com>
2024-12-31 20:37:24 +0100

    fixup! Add day15

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -177,13 +177,277 @@ mod part1 {
     }
 }
 
-fn solve_part2(input: &str) -> i32 {
-    0
+mod part2 {
+    use colored::Colorize;
+
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    enum Item {
+        Empty,
+        Wall,
+        FishLeft,
+        FishRight,
+    }
+
+    #[derive(Debug)]
+    struct Grid {
+        items: Vec<Vec<Item>>,
+        width: i32,
+        height: i32,
+    }
+
+    impl Grid {
+        fn print(&self, sub_x: i32, sub_y: i32) {
+            for y in 0..self.height {
+                for x in 0..self.width {
+                    if x == sub_x && y == sub_y {
+                        let c = "@".green().bold();
+                        print!("{c}");
+                    } else {
+                        let c = match self.items[y as usize][x as usize] {
+                            Item::Empty => ".",
+                            Item::FishLeft => "[",
+                            Item::FishRight => "]",
+                            Item::Wall => "#",
+                        };
+
+                        print!("{c}");
+                    }
+                }
+
+                println!();
+            }
+
+            println!();
+        }
+    }
+
+    #[derive(Debug)]
+    enum Move {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    fn parse_input(input: &str) -> (Grid, Vec<Move>, (i32, i32)) {
+        // Parse grid
+        let mut items = vec![];
+
+        let mut lines = input.lines().enumerate();
+
+        let (_, first) = lines.next().unwrap();
+        let width = first.len() * 2;
+
+        items.push(vec![Item::Wall; width]);
+
+        let mut sub_x = None;
+        let mut sub_y = None;
+
+        for (y, l) in &mut lines {
+            if l == first {
+                items.push(vec![Item::Wall; width]);
+                break;
+            } else {
+                let mut line_items = vec![];
+
+                l.chars().enumerate().for_each(|(x, c)| {
+                    let i = match c {
+                        '.' => [Item::Empty, Item::Empty],
+                        '#' => [Item::Wall, Item::Wall],
+                        'O' => [Item::FishLeft, Item::FishRight],
+                        '@' => {
+                            sub_x = Some((x * 2) as i32);
+                            sub_y = Some(y as i32);
+
+                            [Item::Empty, Item::Empty]
+                        }
+                        _ => panic!("Unexpected item {c}"),
+                    };
+
+                    i.iter().for_each(|&i| line_items.push(i));
+                });
+
+                items.push(line_items);
+            }
+        }
+
+        let height = items.len();
+
+        // Parse moves
+        lines.next(); // Skip empty line
+
+        let mut moves = vec![];
+
+        for (_, l) in lines {
+            l.chars().for_each(|c| {
+                let c = match c {
+                    '^' => Move::Up,
+                    'v' => Move::Down,
+                    '<' => Move::Left,
+                    '>' => Move::Right,
+                    _ => panic!("Unexpected move {c}"),
+                };
+
+                moves.push(c);
+            });
+        }
+
+        (
+            Grid {
+                items,
+                width: width as i32,
+                height: height as i32,
+            },
+            moves,
+            (sub_x.unwrap(), sub_y.unwrap()),
+        )
+    }
+
+    fn can_move(grid: &mut Grid, x: i32, y: i32, dy: i32) -> bool {
+        let next_y = y + dy;
+        let c = match grid.items[next_y as usize][x as usize] {
+            Item::Empty => {
+                true
+            }
+            Item::Wall => {
+                false
+            }
+            Item::FishLeft => {
+                can_move(grid, x, next_y, dy) && can_move(grid, x + 1, next_y, dy)
+            }
+            Item::FishRight => {
+                can_move(grid, x - 1, next_y, dy) && can_move(grid, x, next_y, dy)
+            }
+        };
+
+        c
+    }
+
+    fn do_move(grid: &mut Grid, x: i32, y: i32, dy: i32) -> bool {
+        let next_y = y + dy;
+        let c = match grid.items[next_y as usize][x as usize] {
+            Item::Empty => {
+                true
+            }
+            Item::Wall => {
+                false
+            }
+            Item::FishLeft => {
+                do_move(grid, x, next_y, dy) && do_move(grid, x + 1, next_y, dy)
+            }
+            Item::FishRight => {
+                do_move(grid, x - 1, next_y, dy) && do_move(grid, x, next_y, dy)
+            }
+        };
+
+        if c {
+            grid.items[next_y as usize][x as usize] = grid.items[y as usize][x as usize];
+            grid.items[y as usize][x as usize] = Item::Empty;
+        }
+
+        c
+    }
+
+    pub(super) fn solve(input: &str) -> i32 {
+        let (mut grid, moves, (mut sub_x, mut sub_y)) = parse_input(input);
+
+        grid.print(sub_x, sub_y);
+
+        for m in moves {
+
+            match m {
+                Move::Up | Move::Down => {
+                    let dy = match m {
+                        Move::Up => -1,
+                        Move::Down => 1,
+                        _ => panic!(),
+                    };
+
+                    let c = can_move(&mut grid, sub_x, sub_y, dy);
+                    println!("Can move: {c}");
+                    if c {
+                        do_move(&mut grid, sub_x, sub_y, dy);
+                        sub_y += dy;
+                    }
+                }
+                Move::Left | Move::Right => {
+                    let dx = match m {
+                        Move::Left => -1,
+                        Move::Right => 1,
+                        _ => panic!(),
+                    };
+
+                    let next_x = sub_x + dx;
+
+                    match grid.items[sub_y as usize][next_x as usize] {
+                        Item::Wall => {
+                            continue;
+                        }
+                        Item::Empty => {
+                            sub_x = next_x;
+                        }
+                        Item::FishLeft | Item::FishRight => {
+                            let mut end_x = next_x;
+                            while grid.items[sub_y as usize][end_x as usize] == Item::FishLeft
+                                || grid.items[sub_y as usize][end_x as usize] == Item::FishRight
+                            {
+                                end_x += dx;
+                            }
+
+                            if grid.items[sub_y as usize][end_x as usize] != Item::Empty {
+                                continue;
+                            }
+
+                            if end_x < sub_x {
+                                // Move to the left
+                                for i in end_x..sub_x {
+                                    let item = grid.items[sub_y as usize][(i + 1) as usize];
+                                    if item == Item::Wall {
+                                        continue;
+                                    }
+
+                                    grid.items[sub_y as usize][i as usize] = item;
+                                }
+                            } else {
+                                // Move to the right
+                                for i in (sub_x..=end_x).rev() {
+                                    let item = grid.items[sub_y as usize][(i - 1) as usize];
+                                    if item == Item::Wall {
+                                        continue;
+                                    }
+
+                                    grid.items[sub_y as usize][i as usize] = item;
+                                }
+                            }
+
+                            sub_x = next_x;
+                        }
+                    }
+                }
+            }
+
+            println!("After {m:?}");
+            grid.print(sub_x, sub_y);
+        }
+
+        let mut r = 0;
+        for x in 0..grid.width {
+            for y in 0..grid.height {
+                if grid.items[y as usize][x as usize] != Item::FishLeft {
+                    continue;
+                }
+
+                r += x + y * 100;
+            }
+        }
+
+        r
+    }
 }
 
 fn main() {
     println!("Part 1: {}", part1::solve(INPUT));
-    println!("Part 2: {}", solve_part2(INPUT));
+    println!("Part 2: {}", part2::solve(INPUT));
 }
 
 #[cfg(test)]
@@ -225,21 +489,25 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
-        assert_eq!(part1::solve(TEST_INPUT), 2028);
-        assert_eq!(part1::solve(TEST_INPUT2), 10092);
-        assert_eq!(part1::solve(INPUT), 1413675);
+        //assert_eq!(part1::solve(TEST_INPUT), 2028);
+        //assert_eq!(part1::solve(TEST_INPUT2), 10092);
+        //assert_eq!(part1::solve(INPUT), 1413675);
 
         const TEST_INPUT3: &str = "#######
-#...#.#
 #.....#
-#..OO@#
+#.....#
+#.....#
+#.O#O.#
+#..OO.#
 #..O..#
-#.....#
+#..@..#
 #######
 
-<vv<<^^<<^^";
+>>>>^^<vv<<<^^^^<^^><<^><<^>>v>>><v>>>>";
 
-        //assert_eq!(solve_part2(TEST_INPUT3), 9021);
+        assert_eq!(part2::solve(TEST_INPUT2), 9021);
+
+        // != 1411625
         //assert_eq!(solve_part2(INPUT), 7603);
     }
 }
